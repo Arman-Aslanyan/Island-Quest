ec6a75788460f2d6f0e12d2ad9184d3cae87c9ec
Arman-Aslanyan <67847062+Arman-Aslanyan@users.noreply.github.com>
2021-10-22 14:46:16 -0700

    Basic stuff
    Added PlayerController Script
    Added CameraFollow Script with Lerp

---
+++ Island Quest/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float lerpVal = 0.1f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            //Makes the camera follow the player | With the offset needed to actually see the environment
+            transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(0, 0, -10), lerpVal);
+        }
+    }
+}
---
+++ Island Quest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+        //Gets User input of keys: 'W' 'A' 'S' 'D' and arrow keys
+        float xspeed = Input.GetAxisRaw("Horizontal") * speed;
+        float yspeed = Input.GetAxisRaw("Vertical") * speed;
+
+        //Moves the Player Up/down and/or right/left
+        rb.AddForce(transform.right * xspeed * Time.fixedDeltaTime);
+        rb.AddForce(transform.up * yspeed * Time.fixedDeltaTime);
+    }
+}
